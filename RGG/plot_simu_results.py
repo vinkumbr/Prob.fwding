cefea65d4dfb7f7f7505b0145c6b4ecb070d6955
Vinay <vinkum.br@gmail.com>
2020-06-22 22:43:01 +0530

    making lambda variable in title of plot

--- RGG/plot_simu_results.py
+++ RGG/plot_simu_results.py
@@ -9,13 +9,13 @@ m=101
 with open("simu_results.json") as f:
 	data = json.load(f)
 
-
+lbda = data["lbda"]
 pkndelta_simu = data["pkndelta_simu"]
 tau_kndelta_simu = data["tau_kndelta_simu"]
 tau_kndelta_ergodic = data["tau_kndelta_ergodic"]
 
 fig,(ax1,ax2) = plt.subplots(2,1)
-fig.suptitle('For an (instantiation of) RGG of intensity 4.54 spread in a 101 by 101 grid with connection radius =1. Probabilistic forwarding done with k=20 packets and delta = 0.1')
+fig.suptitle('For an (instantiation of) RGG of intensity %1.2f spread in a 101 by 101 grid with connection radius =1. Probabilistic forwarding done with k=20 packets and delta = 0.1'% lbda)
 ax1.plot(n,pkndelta_simu)
 ax1.set_title('Minimum forwarding probability')
 ax2.plot(n,tau_kndelta_simu,'o-', label='from simulations')
