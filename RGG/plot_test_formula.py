3261eeebd60153a1f4bc266db05b3420a69f88cd
Vinay <vinkum.br@gmail.com>
2020-08-26 09:50:36 +0530

    python and matlab file for tree approx

--- RGG/plot_test_formula.py
+++ RGG/plot_test_formula.py
@@ -1,5 +1,7 @@
 import json
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import interp1d
 
 lbda = 4.5
 m=101
@@ -7,8 +9,13 @@ m=101
 #pkndelta=[0.4,0.5,0.6,0.7,0.8,0.9]
 #trans_from_simu = [17462.9326,22795.9835, 27509.5575, 32125.0597, 36721.5718, 41312.6943]
 
-pkndelta=[0.33,0.34,0.35,0.36,0.37,0.38,0.39,0.4,0.41,0.42,0.43,0.44,0.45,0.46,0.47,0.48,0.49,0.5,0.6,0.7,0.8,0.9]
-trans_from_simu = [ 8291.213,  10936.0183, 12920.1632, 14182.1264, 15172.5103, 16014.7767,16759.6235,17464.9413, 18103.1911, 18618.6264, 19236.1485,19755.0609,20306.1788, 20807.0342, 21296.5837, 21814.6539, 22313.3708, 22817.1542,27509.5575, 32125.0597, 36721.5718, 41312.6943]
+#pkndelta=[0.33,0.34,0.35,0.36,0.37,0.38,0.39,0.4,0.41,0.42,0.43,0.44,0.45,0.46,0.47,0.48,0.49,0.5,0.6,0.7,0.8,0.9]
+#trans_from_simu = [ 8291.213,  10936.0183, 12920.1632, 14182.1264, 15172.5103, 16014.7767,16759.6235,17464.9413, 18103.1911, 18618.6264, 19236.1485,19755.0609,20306.1788, 20807.0342, 21296.5837, 21814.6539, 22313.3708, 22817.1542,27509.5575, 32125.0597, 36721.5718, 41312.6943]
+
+pkndelta = np.arange(0.34,0.361,0.001)
+pkndelta = list(pkndelta)
+#trans_from_simu = [ 8291.213,  8622.3873,  8865.2299,  9158.9303,  9452.261,   9727.877,  9960.1239, 10243.9035, 10574.599,  10675.5662, 10950.1165, 11138.0649, 11393.1084, 11569.2891, 11783.7064, 12060.5172, 12184.5987, 12333.5975, 12487.0412, 12665.7534, 12814.0932, 13031.8681, 13086.6304, 13324.7888, 13413.7331, 13536.3817, 13716.9711, 13816.2633, 13963.9989, 14091.2857, 14151.2738]
+trans_from_simu = [10950.1165, 11138.0649, 11393.1084, 11569.2891, 11783.7064, 12060.5172, 12184.5987, 12333.5975, 12487.0412, 12665.7534, 12814.0932, 13031.8681, 13086.6304, 13324.7888, 13413.7331, 13536.3817, 13716.9711, 13816.2633, 13963.9989, 14091.2857, 14151.2738]
 #Ergodic result
 lbda_pkndelta = [lbda*p for p in pkndelta]
 
@@ -17,10 +24,13 @@ with open("theta_lambda.json") as f:
 	data = json.load(f)
 
 # The lambda values in the file are from 1 to 4.99 in steps of 0.01
+lamb = data["lambda"]
 theta_lambda = data["theta_lambda"]
 theta_lambda_251 = data["theta_lambda_251"]
 theta_lambda_random = data["theta_lambda_random"]
-theta_lbda_kndelta = [theta_lambda_251[int((round(k,2)-1)/0.01)] for k in lbda_pkndelta]
+f = interp1d(lamb,theta_lambda_251)
+theta_lbda_kndelta = f(lbda_pkndelta)
+#theta_lbda_kndelta = [theta_lambda_251[int((round(k,2)-1)/0.01)] for k in lbda_pkndelta]
 theta_lbda_kndelta_square = [i**2 for i in theta_lbda_kndelta]
 
 tau_kndelta_ergodic = [m**2*lbda_pkndelta[i]*theta_lbda_kndelta_square[i] for i in range(len(lbda_pkndelta))]
--- Trees/plot_bintree_approx_results.py
+++ Trees/plot_bintree_approx_results.py
@@ -21,36 +21,39 @@ upper_approx = data['upper_approx']
 trans_upper_approx = data['trans_upper_approx']
 
 fig1,ax1 = plt.subplots(1,1)
-fig1.set_size_inches(5, 2)
+fig1.set_size_inches(5, 2.3)
 #fig.suptitle('Probabilistic forwarding on a binary tree of height 50 with k=100 packets'% H)
-ax1.plot(n,upper_approx,'go', label='upper bound from Prop IV.2',ms=3)
-ax1.plot(n,upper_tight,'rd', label='upper bound from (5)',ms=3)
+ax1.plot(n,upper_approx,'g|', label='upper bound from Prop V.2',ms=4)
+ax1.plot(n,upper_tight,'r:', label='upper bound from (5)',lw=3)
 ax1.plot(n,minp_expr,'b-', label='numerically solving (4)',lw=2)
 ax1.plot(n,lower_tight,'r--', label='lower bound from (6)',lw=2)
-ax1.plot(n,lower_approx,'g+', label='lower bound from Prop IV.1',ms=3)
+ax1.plot(n,lower_approx,'g+', label='lower bound from Prop V.1',ms=2.5)
 
 #ax1.set_title('Minimum retransmission probability')
 ax1.set_ylabel(r'$p_{\ k,n,\delta}$')
 plt.xlabel('Number of coded packets (n)')
 plt.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
 plt.legend(prop={'size': 6})
+plt.tight_layout()
 plt.figure
 #plt.show()
 
 fig2,ax2 = plt.subplots(1,1)
-fig2.set_size_inches(5,2)
-ax2.plot(n,trans_upper_approx,'go', label='upper bound using Prop IV.2',ms=3)
-ax2.plot(n,tau_upper_tight,'rd', label='upper bound using (5)',ms=3)
+fig2.set_size_inches(5,2.3)
+ax2.plot(n,trans_upper_approx,'g|', label='upper bound using Prop V.2',ms=5)
+ax2.plot(n,tau_upper_tight,'r:', label='upper bound using (5)',lw=3)
 ax2.plot(n,trans_expr,'b-', label='numerically solving (4)',lw=2)
 ax2.plot(n,tau_lower_tight,'r--', label='lower bound using (6)',lw=2)
-ax2.plot(n,trans_lower_approx,'g+', label='lower bound using Prop IV.1',ms=3)
+ax2.plot(n,trans_lower_approx,'g+', label='lower bound using Prop V.1',ms=2.5)
 
 #ax2.plot(n,tau_kndelta_ergodic,'+--',label='from theory')
 #ax2.set_title('Expected number of transmissions')
+
 plt.xlabel('Number of coded packets (n)')
 plt.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
 ax2.set_ylabel(r'$\tau_{ k,n,\delta}$')
 
 
 plt.legend(prop={'size': 6})
+plt.tight_layout()
 plt.show()
