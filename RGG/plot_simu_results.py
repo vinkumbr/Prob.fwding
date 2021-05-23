b2d1534ecf858806aa9540af01041b6a5f32f2b9
Vinay <vinkum.br@gmail.com>
2021-05-23 16:46:00 +0530

    ermoving titles in figures while plotting

--- RGG/plot_simu_results.py
+++ RGG/plot_simu_results.py
@@ -20,6 +20,10 @@ tau_kndelta_simu_big = data["tau_kndelta_simu_big"]
 tau_kndelta_ergodic = data["tau_kndelta_ergodic"]
 tau_kndelta_pfs = data["tau_kndelta_pfs"]
 
+tau_kndelta_simu = [tau/4.5 for tau in tau_kndelta_simu]
+tau_kndelta_simu1 = [tau1/4 for tau1 in tau_kndelta_simu1]
+tau_kndelta_pfs = [taupfs/4.5 for taupfs in tau_kndelta_pfs]
+tau_kndelta_ergodic = [erg/4.5 for erg in tau_kndelta_ergodic]
 
 fig1,ax1 = plt.subplots(1,1)
 fig1.set_size_inches(5, 2.5)
@@ -28,8 +32,8 @@ fig1.set_size_inches(5, 2.5)
 #ax1.plot(n,pkndelta_simu1,'+--', label='$\lambda = 4$')
 #ax1.plot(n,pkndelta_simu_big, label=r'$121 \times 121 $')
 ax1.plot(n,pkndelta_simu,'o-', label='from simulations')
-ax1.plot(n,pkndelta_ergodic,'+--', label='from theory')
-ax1.set_title('Minimum forwarding probability')
+ax1.plot(n,pkndelta_ergodic,'+--', label='from heuristics')
+#ax1.set_title('Minimum forwarding probability')
 plt.xlabel('Number of coded packets (n)')
 plt.ylabel(r'$p_{\ k,n,\delta}$')
 plt.tight_layout()
@@ -41,11 +45,11 @@ fig2.set_size_inches(5, 2.5)
 #ax2.plot(n,tau_kndelta_simu1,'+--', label='$\lambda = 4$')
 #ax2.plot(n,tau_kndelta_simu_big, label=r'$121 \times 121$')
 ax2.plot(n,tau_kndelta_simu,'o-', label='from simulations')
-ax2.plot(n,tau_kndelta_ergodic,'+--',label='from theory')
-#ax2.plot(n,tau_kndelta_pfs,'+--',label='using prob from simu')
-ax2.set_title('Expected number of transmissions')
+ax2.plot(n,tau_kndelta_ergodic,'+--',label='from heuristics')
+#ax2.plot(n,tau_kndelta_pfs,'+--',label='using (9) with $p_{k,n,\delta}$ from Fig. 1(a)')
+#ax2.set_title('Expected number of transmissions')
 plt.xlabel('Number of coded packets (n)')
-plt.ylabel(r'$\tau_{\ k,n,\delta}\ / \ m^2$')
+plt.ylabel(r'$\tau_{\ k,n,\delta}\ / \ \lambda m^2$')
 plt.tight_layout()
 ax2.legend()
 plt.show()
